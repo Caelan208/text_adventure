75aab2653b70db8752b31531fea065f976290546
Caelan208 <c.verhoef@student.alfa-college.nl>
2026-01-19 09:30:21 +0100

    health bar

--- Zuul/src/Player.cs
+++ Zuul/src/Player.cs
@@ -1,10 +1,13 @@
 class Player
 {
-// auto property
-public Room CurrentRoom { get; set; }
-// constructor
-public Player()
-{
-CurrentRoom = null;
-}
+	// fields
+	private int health;
+	// auto property
+	public Room CurrentRoom { get; set; }
+	// constructor
+	public Player()
+	{
+		health = 100;
+		CurrentRoom = null;
+	}
 }
